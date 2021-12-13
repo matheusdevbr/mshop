85329a6cf33f52d2919475ab077c63d4df11abeb
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-12-13 16:18:42 -0300

    Add responsividade

--- src/components/Content/style.ts
+++ src/components/Content/style.ts
@@ -3,7 +3,7 @@ import styled  from "styled-components";
 export const Container = styled.main`
 
   display: grid;
-  grid-template-columns: repeat( 3, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   text-align: center;
   gap: 2rem;
  
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -3,7 +3,12 @@ import styled from "styled-components";
 export const Container = styled.div`
  display: grid;
     grid-template-columns: 1fr 3fr;
-
+    grid-gap: 1rem;
     
     padding: 4rem;
+
+    @media (max-width: 500px) {
+        grid-template-columns: 1fr;
+    grid-gap: 1rem;
+    }
 `
